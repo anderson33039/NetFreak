377c57e5257be19683996bbe2face19a233ce628
anderson33039 <anderson.j4907@gmail.com>
2024-12-15 11:38:02 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -10,17 +10,3 @@ document.getElementById('enter').addEventListener('click', function() {
     localStorage.setItem('userData', JSON.stringify(userData));
 })
 
-function saveReceipt() {
-    var storedData =localStorage.getItem('userData');
-    var userData = JSON.parse(storedData);
-    var meow = localStorage.getItem('product');
-    var arf = localStorage.getItem('revise');
-    var price = localStorage.getItem('price');
-
-    const blob = new Blob(["Name: " + userData.name + "\nGrade/Section: " + userData.grsc + "\n\nOrder: " + meow + "(" + arf + ")\nPrice: " + price], {type: 'text/plain'});
-
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'receipt.txt';
-    link.click();
-}
